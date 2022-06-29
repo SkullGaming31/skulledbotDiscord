b05cc18d6ffb5cc23e05b07ed903987fcbfe725d
SkullGaming31 <canadianfan11@hotmail.com>
2022-06-29 02:25:00 -0230

    styled guild left message

--- Events/guildMember/guildMemberRemove.js
+++ Events/guildMember/guildMemberRemove.js
@@ -1,4 +1,4 @@
-const { GuildMember } = require('discord.js');
+const { GuildMember, MessageEmbed } = require('discord.js');
 module.exports = {
 	name: 'guildMemberRemove',
 	/**
@@ -12,13 +12,18 @@ module.exports = {
 		const personalDiscord = guild.channels.cache.get('838158641072832562');// personal discord Logs Channel ID
 		const daydDiscord = guild.channels.cache.get('989110717837873172');// dayd discord Logs Channel ID
 
+		const embed = new MessageEmbed()
+			.setTitle('MEMBER LEFT')
+			.setColor('RED')
+			.setDescription(`\`${member}\` left the server`);
+
 		try {
 			switch (guild.id) {
 				case '985309600347717672': // DAYD discord
-					if (daydDiscord.isText()) await daydDiscord.send({ content: `${member} left the server` });
+					if (daydDiscord.isText()) await daydDiscord.send({ embeds: [embed] });
 					break;
 				case '797740303176040498':
-					if (personalDiscord.isText()) await personalDiscord.send({ content: `${member} left the server` });
+					if (personalDiscord.isText()) await personalDiscord.send({ embeds: [embed] });
 					break;
 			}
 		} catch (err) { console.error(err); }
